42e97e1f80630c0c094af728d37d31dc4e12583b
Seamus Conlon <seamus_conlon27@hotmail.co.uk>
2023-03-24 13:19:40 +0000

    doing my homework, again

--- httprequests/views.py
+++ httprequests/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def home(request):
+    return render(request, "httprequests/home.html")
+
+def hello(request):
+    return render(request, "httprequests/hello.html")
