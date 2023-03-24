1b6f20fd7e1e4daa735b1b2821cb93d175d7d9e5
Seamus Conlon <seamus_conlon27@hotmail.co.uk>
2023-03-24 14:50:19 +0000

    testy testy

--- httprequests/views.py
+++ httprequests/views.py
@@ -3,8 +3,19 @@ from django.http import HttpResponse
 
 # Create your views here.
 
+class testy():
+    def __init__(self,name,number):
+        self.name = name
+        self.number = number
+
+    def conc():
+        return str(self.name)+str(self.number)
+
 def home(request):
     return render(request, "httprequests/home.html")
 
 def hello(request):
-    return render(request, "httprequests/hello.html")
+    the_name = request.GET.get('name')
+    the_number = request.GET.get('number')
+    test = testy(the_name,the_number)
+    return render(request, "httprequests/hello.html", {'name':the_name,'number':the_number})
